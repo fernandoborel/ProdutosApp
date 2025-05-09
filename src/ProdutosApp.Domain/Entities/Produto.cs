93d00d524be2076bbc49b90074a1bfb95f7b3607
fernando borel <fernandomenezesborel@gmail.com>
2025-05-09 18:52:49 -0300

    Feat. Adicionado conexão com o banco de dados e adicionado docker ao projeto

---
+++ src/ProdutosApp.Domain/Entities/Produto.cs
@@ -0,0 +1,22 @@
+﻿namespace ProdutosApp.Domain.Entities;
+
+public class Produto
+{
+    #region Propriedades
+    
+    public Guid? Id { get; set; }
+    public string? Nome { get; set; }
+    public decimal? Preco { get; set; }
+    public int? Quantidade { get; set; }
+    public DateTime? DataHoraCriacao { get; set; }
+    public bool? Ativo { get; set; }
+    public Guid? CategoriaId { get; set; }
+    
+    #endregion
+    
+    #region Relacionamentos
+    
+    public Categoria? Categoria { get; set; }
+    
+    #endregion
+}
